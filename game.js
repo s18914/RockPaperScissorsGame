409a9aebc8532a0c424b09747668c5fbda9422b6
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2022-03-25 19:35:48 +0100

    start project - player's choice

---
+++ game.js
@@ -0,0 +1,26 @@
+// https://websamuraj.pl/examples/js/projekt7/
+
+const gameSummary = {
+ numbers: 0,
+ wins: 0,
+ losses: 0,
+ draws: 0,
+}
+
+const game = {
+ playerHand: "",
+ aiHand: "",
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+// Pierwsza funkcja
+function handSelection() {
+
+ game.playerHand = this.dataset.option
+ console.log(game.playerHand);
+ hands.forEach(hand => hand.style.boxShadow = '');
+ this.style.boxShadow = '0 0 0 4px red';
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
