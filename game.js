b3981f19e4870ecab8ed9c12c14ac47c656c164e
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2022-03-26 19:28:14 +0100

    ai choice, check result

--- game.js
+++ game.js
@@ -14,6 +14,8 @@ const game = {
 
 const hands = [...document.querySelectorAll('.select img')];
 
+const btnStart = document.querySelector('.start');
+
 function handSelection() {
  game.playerHand = this.dataset.option
  console.log(game.playerHand);
@@ -21,4 +23,38 @@ function handSelection() {
  this.style.boxShadow = '0 0 0 5px gold';
 }
 
+function checkResult() {
+    if (game.layerHand === game.aiHand) {
+        //gameSummary.draws++;
+        return 'Draw!'
+    } else if (game.playerHand === 'stone' && game.aiHand === 'scissors' 
+    || game.playerHand === 'scissors' && game.aiHand === 'paper' 
+    || game.playerHand === 'paper' && game.aiHand === 'stone') {
+        //game.Summary.wins++;
+        return "You win!";
+    } else {
+        //game.Summary.losses++;
+        return "You loose..."
+    }
+}
+
+function computerChoice() {
+    return hands[Math.floor(Math.random()*hands.length)].dataset.option;
+}
+
+function play() {
+
+    if (game.playerHand === "") {
+        alert('Select rock paper or scissors');
+        return;
+    }
+
+    game.aiHand = computerChoice();
+    
+    checkResult();
+    gameSummary.numbers++;
+    
+}
+
 hands.forEach(hand => hand.addEventListener('click', handSelection));
+btnStart.addEventListener('click', play);
