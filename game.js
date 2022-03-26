413c3e25740a14289148e9e6448ea53497865587
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2022-03-26 18:53:10 +0100

    styling

--- game.js
+++ game.js
@@ -4,23 +4,21 @@ const gameSummary = {
  numbers: 0,
  wins: 0,
  losses: 0,
- draws: 0,
+ draws: 0
 }
 
 const game = {
  playerHand: "",
- aiHand: "",
+ aiHand: ""
 }
 
 const hands = [...document.querySelectorAll('.select img')];
 
-// Pierwsza funkcja
 function handSelection() {
-
  game.playerHand = this.dataset.option
  console.log(game.playerHand);
  hands.forEach(hand => hand.style.boxShadow = '');
- this.style.boxShadow = '0 0 0 4px red';
+ this.style.boxShadow = '0 0 0 5px gold';
 }
 
-hands.forEach(hand => hand.addEventListener('click', handSelection))
+hands.forEach(hand => hand.addEventListener('click', handSelection));
