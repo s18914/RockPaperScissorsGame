e436027b746a75e5c6c6065f1314605d4c632d31
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2022-03-28 17:37:03 +0200

    project complete

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-// https://websamuraj.pl/examples/js/projekt7/
-
 const gameSummary = {
  numbers: 0,
  wins: 0,
@@ -18,28 +16,36 @@ const btnStart = document.querySelector('.start');
 
 function handSelection() {
  game.playerHand = this.dataset.option
- console.log(game.playerHand);
  hands.forEach(hand => hand.style.boxShadow = '');
  this.style.boxShadow = '0 0 0 5px gold';
 }
 
 function checkResult() {
-    if (game.layerHand === game.aiHand) {
-        //gameSummary.draws++;
-        return 'Draw!'
-    } else if (game.playerHand === 'stone' && game.aiHand === 'scissors' 
-    || game.playerHand === 'scissors' && game.aiHand === 'paper' 
-    || game.playerHand === 'paper' && game.aiHand === 'stone') {
-        //game.Summary.wins++;
+
+    if (game.playerHand === game.aiHand) {
+        gameSummary.draws += 1;
+        document.querySelector('p.draws span').textContent = gameSummary.draws;
+        return "Draw!"
+    } else if (game.playerHand === "stone" && game.aiHand === "scissors" 
+        || game.playerHand === "scissors" && game.aiHand === "paper" 
+        || game.playerHand === "paper" && game.aiHand === "stone") {
+        gameSummary.wins += 1;
+        document.querySelector('p.wins span').textContent = gameSummary.wins;
         return "You win!";
     } else {
-        //game.Summary.losses++;
+        gameSummary.losses++;
+        document.querySelector('p.losses span').textContent = gameSummary.losses;
         return "You loose..."
     }
 }
 
 function computerChoice() {
     return hands[Math.floor(Math.random()*hands.length)].dataset.option;
+}
+
+function resetGame() {
+    game.playerHand = "";
+    hands.forEach(hand => hand.style.boxShadow = '');
 }
 
 function play() {
@@ -49,11 +55,18 @@ function play() {
         return;
     }
 
+    document.querySelector('[data-summary="your-choice"]').textContent = game.playerHand;
+
     game.aiHand = computerChoice();
+    document.querySelector('[data-summary="ai-choice"]').textContent = game.aiHand;
     
-    checkResult();
+    
+    document.querySelector('[data-summary="who-win"]').textContent = checkResult();
+
     gameSummary.numbers++;
+    document.querySelector('p.numbers span').textContent = gameSummary.numbers;
     
+    resetGame();
 }
 
 hands.forEach(hand => hand.addEventListener('click', handSelection));
